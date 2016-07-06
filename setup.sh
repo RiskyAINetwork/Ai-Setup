cd6958d036d2da4e86484d89d68b5b034fce1d0f
crazywolf132 <crazywolf132@gmail.com>
2016-07-06 23:41:23 +1000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo 'System is MAC'
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install python
+    brew install git
+    echo 'Now going to install the required python packets.'
+    git clone https://github.com/gitpython-developers/GitPython.git
+    cd GitPython
+    python setup.py install
+    cd ../
+    rm -rf GitPython
+    echo 'Should be done.'
+    clear
+    echo 'Cloning AI.'
+    git clone https://github.com/AI
+    echo 'Done.'
+    cd AI
+    print 'Now going to install what is needed.'
+    python start.py
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo 'System is LINUX'
+    apt-get install -y python
+    echo 'Python installed.'
+    echo 'Installing git.'
+    apt-get install -y git
+    echo 'Git is now installed.'
+    echo 'Now going to install the required python packets.'
+    git clone https://github.com/gitpython-developers/GitPython.git
+    cd GitPython
+    python setup.py install
+    cd ../
+    rm -rf GitPython
+    echo 'Should be done.'
+    clear
+    echo 'Cloning AI.'
+    git clone https://github.com/AI
+    echo 'Done.'
+    cd AI
+    print 'Now going to install what is needed.'
+    python start.py
+    
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo 'System is WINDOWS'
+fi
