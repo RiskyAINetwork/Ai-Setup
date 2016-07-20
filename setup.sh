cbcf546b9587021b5d333ab8e143b1b3a01176ae
crazywolf132 <crazywolf132@gmail.com>
2016-07-20 12:30:08 +1000

    Added some changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -28,18 +28,97 @@ if [ "$(uname)" == "Darwin" ]; then
     printf "%s\n${blu}User is on MAC...${end}\n"
     # CD to root directory where the AI will be installed.
     cd
+    # Print our logo.
+    cat << "EOF"
+    R.A.I.N
+
+    01010010 00101110 01000001 00101110 01001001 00101110 01001110
+
+
+
+
+    Risky Artificial Intelligence Network.
+
+    01010010 01101001 01110011 01101011 01111001 00100000 01000001 01110010 01110100 01101001 01100110 01101001 01100011 01101001 01100001 01101100 00100000 01001001 01101110 01110100 01100101 01101100 01101100 01101001 01100111 01100101 01101110 01100011 01100101 00100000 01001110 01100101 01110100 01110111 01101111 01110010 01101011 00101110
+
+    EOF
     # Check for folder.
     if [[ -d "Samantha" && ! -L "Samantha" ]]; then
         # Check for INSTALL file in directory.
         if [ -e "INSTALL" ]; then
             # If the INSTALL file exists. Continue with the install.
+            # Going to check for permission.
+            printf "%sThis script will install the Samantha AI and it's dependencies.\n"
+            # Ensure the use would like to start the install
+            read -r -p "Would you like to continue? [y/N] " response
+            # Grab the response from the user input.
+            if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+                # If the user agrees with it.
+                printf "%sExcellent! ${red}Please do not exit this script until it is complete.${end}\n"
+            else
+                # Seen the user does not agree we are going to exit.
+                exit 1
+            fi
+            # Going forth with the install if they responded with yes.
+            # Going to check if any of the install proccess files exist.
+            if [ -e "PART 1" ]; then
+              # Seen as this file exists then we will run the installer from here.
+            else
+              if [ -e "PART 2" ]; then
+                # The installer has gotten this far before. Just pickup from here.
+              else
+                if [ -e "PART 3" ]; then
+                  # The installer has gotten this far before. Just pick up from here.
+                else
+                  if [ -e "PART 4" ]; then
+                    # The installer has gotten this far before. Just pick up from here.
+                  else
+                    curl -L "" > PART 1
+                    curl -L "" > PART 2
+                    curl -L "" > PART 3
+                    curl -L "" > PART 4
+                  fi
+                fi
+              fi
+            fi
+
+
+
+
+
+
+
         else
-            # Download the INSTALL file incase the installer does not finish.
+            # Clearly the user already has the AI installed.
         fi
     else
         # Make a directory to contain the AI seen as it does not exist.
         mkdir Samantha
     fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       # Detect if they are in root mode.
@@ -52,13 +131,41 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       printf "%s\n${blu}User is on LINUX...${end}\n"
       # CD to root directory where the AI will be installed
       cd
+      # Print our logo.
+      cat << "EOF"
+      R.A.I.N
+
+      01010010 00101110 01000001 00101110 01001001 00101110 01001110
+
+
+
+
+      Risky Artificial Intelligence Network.
+
+      01010010 01101001 01110011 01101011 01111001 00100000 01000001 01110010 01110100 01101001 01100110 01101001 01100011 01101001 01100001 01101100 00100000 01001001 01101110 01110100 01100101 01101100 01101100 01101001 01100111 01100101 01101110 01100011 01100101 00100000 01001110 01100101 01110100 01110111 01101111 01110010 01101011 00101110
+
+      EOF
       # Check for folder.
       if [[ -d "Samantha" && ! -L "Samantha" ]]; then
         # Check for INSTALL file in directory.
         if [ -e "INSTALL" ]; then
           # If the INSTALL file exists. Contintue with the install.
+          # Going to check for permission.
+          printf "%sThis script will install the Samantha AI and it's dependencies.\n"
+          # Ensure the use would like to start the install
+          read -r -p "Would you like to continue? [y/N] " response
+          # Grab the response from the user input.
+          if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+              # If the user agrees with it.
+              printf "%sExcellent! ${red}Please do not exit this script until it is complete.${end}\n"
+          else
+              # Seen the user does not agree we are going to exit.
+              exit 1
+          fi
+          # Going forth with the install if they responded with yes.
+
         else
-          # Download the INSTALL file incase the installer does not finish.
+          # Clearly the user already has the AI installed.
         fi
       else
         # Make a directory to contain the AI seen as it does not exist.
